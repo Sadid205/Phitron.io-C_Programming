c529a4aea9fcc0be5469160a327281746d78d512
Abdullah-Al-Sadid <abdullahalsadid1914@gmail.com>
2023-11-03 11:48:19 +0600

    initial commit

---
+++ zero_matrix.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+     int row,col;
+     scanf("%d %d",&row,&col);
+     int a[row][col];
+     int element = row * col;
+     int count = 0;
+     for(int i=0;i<row;i++)
+     {
+        for(int j=0;j<col;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+     }
+
+      for(int i=0;i<row;i++)
+     {
+        for(int j=0;j<col;j++)
+        {
+            if(a[i][j]==0)
+            {
+                count++;
+            }
+        }
+     }
+
+     if(element==count)
+     {
+        printf("Zero matrix\n");
+     }
+     else 
+     {
+        printf("Not Zero matrix\n");
+     }
+
+    return 0;
+}
